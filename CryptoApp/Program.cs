c032cc552bd978fa9106ed25ebba18de588321c1
dgaliciag <dgaliciag@LAP-DGALICIA>
2018-04-30 09:15:50 -0500

    Comentario de prueba

--- CryptoApp/Program.cs
+++ CryptoApp/Program.cs
@@ -17,6 +17,7 @@ namespace CryptoApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CryptFrm());
+            //Cambio
         }
     }
 }
